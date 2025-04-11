1bd4c395469e7aca577eb3383499ea60d168bbde
Dylan McKeon <mckeon.dylant@gmail.com>
2025-04-10 22:54:17 -0500

    updated explore

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,21 +3,19 @@ import Card from "../components/Card";
 import data from "../assets/data";
 
 const Explore = () => {
-  const sneakers = data.sneakers;
+  // Use products from data object
+  const products = data.products;
 
-  const filteredItems = sneakers.filter(
-    (s) => s.retail_price_cents !== null && s.story_html !== null
-  );
-
-  const items = filteredItems.map((item) => {
+  // Map products to include quantity
+  const items = products.map((item) => {
     return { ...item, qty: 1 };
   });
 
   return (
     <div className="">
       <div className="w-full min-h-fit p-10 md:p-20 grid grid-cols-1 gap-y-6 md:grid-cols-2 md:gap-x-6 lg:grid-cols-3 lg:gap-8 xl:grid-cols-4 xl:gap-10mx-auto ">
-        {items.map((shoe, idx) => (
-          <Card key={shoe.id} shoe={shoe} />
+        {items.map((product) => (
+          <Card key={product.id} product={product} />
         ))}
       </div>
     </div>
